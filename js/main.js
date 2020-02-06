4d2acb34f9ddad468f1f7231bba2e412fdb0b442
David Lane <fastlane27@gmail.com>
2020-02-06 16:51:40 -0600

    Enter key functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-$('button').on('click', function(evt) {
+$('button').on('click', createLi);
+$('input').on('keydown', function(evt) {
+    if (evt.keyCode === 13) createLi();
+});
+
+$('ul').on('click', 'span', function() {
+    $(this).closest('li').fadeOut(200, function() {
+        $(this).remove();
+    });
+});
+
+function createLi() {
     if ($('input').val() === '') return;
     let liTemplate = `<li><span>X</span>${$('input').val()}</li>`;
     $('ul').append(liTemplate);
     $('li').last().hide().fadeIn(200);
     $('input').val('');
-});
-
-$('ul').on('click', 'span', function(evt) {
-    $(this).closest('li').fadeOut(200, function() {
-        $(this).remove();
-    });
-});
+}
